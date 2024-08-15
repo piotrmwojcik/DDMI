8fd5d26da5e4cf105a5ff1e772011c27f2adadd0
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-08-16 00:35:18 +0200

    added configs

--- models/transformer.py
+++ models/transformer.py
@@ -516,9 +516,6 @@ class Transformer(nn.Module):
         else:
             inp = [x, t_embedding]
         inp = torch.cat(inp, 1)
-        print('!!!')
-        print(x.shape)
-        print(inp.shape)
         output = self.decoder(inp)
         # TODO: Global residual connection:
         if self.use_global_residual:
