a90894acd50a40b1abf9a69b2423d54c11de5ecd
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-08-16 00:16:10 +0200

    added configs

--- tools/ldm/image_ss.py
+++ tools/ldm/image_ss.py
@@ -124,7 +124,7 @@ class LDMSSTrainer(object):
     def train(self):
         device = self.accelerator.device
         self.diffusion_process.train()
-        shape = (self.test_batch_size, self.channels)
+        shape = (self.test_batch_size, 50307)
         noise_fix = torch.randn((self.test_batch_size, self.channels), device = device)
 
         cache = {}
