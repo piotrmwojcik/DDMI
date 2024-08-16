eb959f2058e86df4a6f3f514a4497834bca37874
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-08-16 11:13:35 +0200

    added configs

--- tools/ldm/image_ss.py
+++ tools/ldm/image_ss.py
@@ -156,7 +156,7 @@ class LDMSSTrainer(object):
                         combined_parameters += list(_code.parameters())
                     optim = torch.optim.Adam(lr=1e-4, params=combined_parameters)
 
-                    for i in range(250):
+                    for i in range(500):
                         model_output = []
                         with self.accelerator.autocast():
                             for _code in _mlp_list:
