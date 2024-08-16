2a823690a8c15ca03c82571aa1d083dde8f90a8d
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-08-16 10:28:35 +0200

    added configs

--- tools/ldm/image_ss.py
+++ tools/ldm/image_ss.py
@@ -164,6 +164,7 @@ class LDMSSTrainer(object):
                                 model_output.append(mo)
                             model_output = torch.cat(model_output, dim=0)
                             loss = ((model_output - x) ** 2).mean()
+                            print(loss)
                             self.accelerator.backward(loss)
 
                             optim.zero_grad()
