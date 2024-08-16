6373f53cb738b5c1c5aa93d159021af117cb5994
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-08-16 02:24:20 +0200

    added configs

--- tools/ldm/image_ss.py
+++ tools/ldm/image_ss.py
@@ -137,7 +137,9 @@ class LDMSSTrainer(object):
                     #y = y.clamp(-1., 1.)
                     #b, c, h, w = x.shape
                     x = x.permute(0, 2, 3, 1).view(5, 128*128, 3)
+                    print(get_mgrid(128, dim=2).shape)
                     input = get_mgrid(128, dim=2).cuda().unsqueeze(0)
+
                     input = input.repeat(5, 1, 1)
 
                     _mlp_list = []
