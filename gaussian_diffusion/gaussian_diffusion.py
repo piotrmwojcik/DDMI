05f420be55802962261ae51309d7684f7262987d
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-08-19 23:56:40 +0200

    Added Gaussian diffusion

--- gaussian_diffusion/gaussian_diffusion.py
+++ gaussian_diffusion/gaussian_diffusion.py
@@ -778,7 +778,7 @@ class GaussianDiffusion:
             if self.loss_type == LossType.RESCALED_KL:
                 terms["loss"] *= self.num_timesteps
         elif self.loss_type == LossType.MSE or self.loss_type == LossType.RESCALED_MSE:
-            model_output = model(x_t, self._scale_timesteps(t), **model_kwargs)
+            model_output = self.model(x_t, self._scale_timesteps(t), **model_kwargs)
 
             if self.model_var_type in [
                 ModelVarType.LEARNED,
